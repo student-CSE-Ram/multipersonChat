fedd2d916fa7ea6d58360f118f15645bc536f6cc
student-CSE-Ram <rampc45.07@gmail.com>
2025-12-14 12:17:16 +0530

    the complete multiperson chat app

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,12 +18,11 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
+  credentials: true
+}));
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
